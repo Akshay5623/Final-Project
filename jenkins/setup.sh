5a54e4acfb96dc53671b43ef6cde99d9581cadf9
Ubuntu <akshay@JenkinsServer.hfasecb5lc3u5g10wgppxvdszh.zx.internal.cloudapp.net>
2021-12-31 13:35:26 +0000

    edit to setup

--- jenkins/setup.sh
+++ jenkins/setup.sh
@@ -13,7 +13,7 @@ newgrp docker
 
 #install docker compose
 version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
-sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 # docker login
