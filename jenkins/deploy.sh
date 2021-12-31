4b3c6f3ab7bec6ee0b1aad0fee22167e03a06a9a
Ubuntu <akshay@JenkinsServer.hfasecb5lc3u5g10wgppxvdszh.zx.internal.cloudapp.net>
2021-12-31 15:28:41 +0000

    revert

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -5,4 +5,5 @@ echo "Deploy Stage"
 scp docker-compose.yaml jenkins@ProductionServer:/home/jenkins/docker-compose.yaml 
 ssh jenkins@ProductionServer \
     DOCKER_HUB_CREDS_USR=$DOCKER_HUB_CREDS_USR \
+    DATABASE_URI=$DATABASE_URI \
     docker stack deploy --compose-file docker-compose.yaml fantasybasketball_app 
