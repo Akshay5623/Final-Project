2296400b78482bc3ad7acc7f87f359bd09b8f86f
Ubuntu <akshay@JenkinsServer.hfasecb5lc3u5g10wgppxvdszh.zx.internal.cloudapp.net>
2021-12-31 14:27:34 +0000

    deploy stage

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 echo "Deploy Stage"
+
+scp docker-compose.yaml jenkins@ProductionServer:/home/jenkins/docker-compose.yaml 
+ssh jenkins@ProductionServer \
+    DOCKER_HUB_CREDS_USR=$DOCKER_HUB_CREDS_USR \
+    docker stack deploy --compose-file docker-compose.yaml fantasybasketball_app 
