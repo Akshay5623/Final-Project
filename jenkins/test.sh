6c83755ac48de8c90d3409c4b9555af7ef9956b1
Ubuntu <akshay@JenkinsServer.hfasecb5lc3u5g10wgppxvdszh.zx.internal.cloudapp.net>
2021-12-31 16:04:37 +0000

    adding test stage

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 
 echo "Test Stage"
+
+# install and activate virtual environment
+python3 -m venv venv
+source venv/bin/activate
+
+# install pip dependencies
+pip3 install -r requirements.txt
+
+# make folder for test reports
+mkdir test_reports
+
+#run pytest
+python3 -m pytest \
+    --cov=application \
+    --cov-report term-missing \
+    --cov-report xml:test_reports/coverage.xml \
+    --junitxml=test_reports/junit_report.xml
+
+# deactivate and remove venv
+deactivate
+rm -rf venv
