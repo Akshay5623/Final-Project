21d20bbf1f5139d860efb22e9488abb7ac65ea3b
Akshay5623 <93129914+Akshay5623@users.noreply.github.com>
2022-01-03 17:22:17 +0000

    Commit to Main Branch (#16)
    * added git ignore file
    
    * Feature/1 create and read team and players (#5)
    
    * create and read functions
    
    * create and read team and players
    
    Co-authored-by: Ubuntu <akshay@JenkinsServer.hfasecb5lc3u5g10wgppxvdszh.zx.internal.cloudapp.net>
    
    * update team name and players (#6)
    
    Co-authored-by: Ubuntu <akshay@JenkinsServer.hfasecb5lc3u5g10wgppxvdszh.zx.internal.cloudapp.net>
    
    * delete team and players (#7)
    
    Co-authored-by: Ubuntu <akshay@JenkinsServer.hfasecb5lc3u5g10wgppxvdszh.zx.internal.cloudapp.net>
    
    * adding pytest scripts (#9)
    
    Co-authored-by: Ubuntu <akshay@JenkinsServer.hfasecb5lc3u5g10wgppxvdszh.zx.internal.cloudapp.net>
    
    * adding base files (#10)
    
    Co-authored-by: Ubuntu <akshay@JenkinsServer.hfasecb5lc3u5g10wgppxvdszh.zx.internal.cloudapp.net>
    
    * adding dockerfile (#13)
    
    Co-authored-by: Ubuntu <akshay@JenkinsServer.hfasecb5lc3u5g10wgppxvdszh.zx.internal.cloudapp.net>
    
    * Feature/jenkins ci cd (#15)
    
    * adding base jenkins pipeline files
    
    * setup and build stage
    
    * compose for build stage
    
    * remove database uri
    
    * edit to setup
    
    * adding push stage
    
    * deploy stage
    
    * adding database uri credentials
    
    * database uri edit
    
    * trying to fix deploy stage
    
    * trying to fix deploy stagee
    
    * revert
    
    * test
    
    * reverting
    
    * adding test stage
    
    * edit jenkinsfilee
    
    * edit test stage
    
    * edit dockercompose
    
    * adding ports
    
    * change
    
    * edit testing
    
    * fixing docker-compose
    
    * Showing Webhook
    
    * showing webhoook
    
    * webhook
    
    * Create docs
    
    * Delete docs
    
    * Create fill
    
    * Add files via upload
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md

---
+++ application/models.py
@@ -0,0 +1,16 @@
+from application import db
+
+class Teams(db.Model):
+    team_id = db.Column(db.Integer, primary_key=True)
+    teamname = db.Column(db.String(255), nullable=False)
+    city = db.Column(db.String(255), nullable=False)
+    players = db.relationship('Players', cascade="all", backref='team') # Cascade all allows for deletion of players by deleting the team too.
+
+class Players(db.Model):
+    player_id = db.Column(db.Integer, primary_key=True)
+    pointguard = db.Column(db.String(255), nullable=False)
+    shootingguard = db.Column(db.String(255), nullable=False)
+    smallforward = db.Column(db.String(255), nullable=False)
+    powerforward = db.Column(db.String(255), nullable=False)
+    center = db.Column(db.String(255), nullable=False)
+    team_id = db.Column(db.Integer, db.ForeignKey('teams.team_id'), nullable=False)
---
+++ application/routes.py
@@ -0,0 +1,80 @@
+from application import app, db
+from application.models import Teams, Players
+from application.forms import TeamForm, PlayersForm
+from flask import render_template, request, url_for, redirect, jsonify, Response
+
+@app.route('/')
+def home():
+    all_teams = Teams.query.all()
+    return render_template("index.html", title="Home", all_teams=all_teams)
+
+@app.route('/create-team', methods=["GET","POST"])
+def create_team():
+    form = TeamForm()
+    form.submit.label.text= "Add Team Name and City"
+    
+    if request.method == "POST":
+        new_team = Teams(teamname=form.teamname.data, city=form.city.data)
+        db.session.add(new_team)
+        db.session.commit()
+        return redirect(url_for("home")) 
+
+    return render_template("team_form.html", title="Create Your Team Name", form=form)
+
+@app.route('/update-team/<int:team_id>', methods=["GET", "POST"])
+def update_team(team_id):
+    team = Teams.query.get(team_id)    
+    form = TeamForm(teamname=team.teamname, city=team.city)
+    form.submit.label.text="Update Team Name and City"
+
+    if request.method=='POST':
+        team.teamname = form.teamname.data
+        team.city = form.city.data
+        db.session.commit()
+        return redirect(url_for("home"))
+
+    return render_template("team_form.html", title="Update Team Name and City", form=form)
+
+@app.route('/delete-team/<int:team_id>')
+def delete_team(team_id):
+    team = Teams.query.get(team_id)
+    db.session.delete(team)
+    db.session.commit()
+    return redirect(url_for('home'))
+
+@app.route('/create-players/<int:team_id>', methods=['GET','POST'])
+def create_players(team_id):
+    form = PlayersForm()
+    form.submit.label.text = "Add Players"
+
+    if request.method=='POST':
+        new_players = Players(pointguard=form.pointguard.data, shootingguard=form.shootingguard.data, smallforward=form.smallforward.data, powerforward=form.powerforward.data, center=form.center.data, team_id=team_id)
+        db.session.add(new_players)
+        db.session.commit()
+        return redirect(url_for("home"))
+
+    return render_template("players_form.html", title="Add your Players", form=form)
+
+@app.route('/update-players/<int:player_id>', methods=['GET','POST'])
+def update_players(player_id):
+    player= Players.query.get(player_id)
+    form = PlayersForm(pointguard=player.pointguard, shootingguard=player.shootingguard, smallforward=player.smallforward, powerforward=player.powerforward, center=player.center)
+    form.submit.label.text="Update your Players"
+
+    if request.method=='POST':
+        player.pointguard = form.pointguard.data
+        player.shootingguard = form.shootingguard.data
+        player.smallforward = form.smallforward.data
+        player.powerforward = form.powerforward.data
+        player.center = form.center.data
+        db.session.commit()
+        return redirect(url_for("home"))
+
+    return render_template("players_form.html", title="Update your Players", form=form)
+
+@app.route('/delete-players/<int:player_id>')
+def delete_players(player_id):
+    player = Players.query.get(player_id)
+    db.session.delete(player)
+    db.session.commit()
+    return redirect(url_for("home"))    
